e6c94355a4684192e75e3848ced1c6d3e8ba5e61
zdsdd <sebasadowy@gmail.com>
2026-08-11 21:47:44 +0200

    5.4

--- internal/store/store.go
+++ internal/store/store.go
@@ -111,8 +111,7 @@ func (s *Store) Lookup(_ context.Context, short string) (string, error) {
 		return "", ErrNotFound
 	}
 	if err != nil {
-		s.logger.Error("failed to read file", slog.String("file", shortcodeFilepath), slog.Any("error", err))
-		return "", err
+		return "", fmt.Errorf("read %s: %w", shortcodeFilepath, err)
 	}
 	return string(data), nil
 }
