9d406398526b81f48f14ab3df1b725f8a4dd769b
zdsdd <sebasadowy@gmail.com>
2026-08-15 21:48:02 +0200

    6.2

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"syscall"
 	"time"
 
+	"boot.dev/linko/internal/build"
 	"boot.dev/linko/internal/linkoerr"
 	"boot.dev/linko/internal/store"
 	pkgerr "github.com/pkg/errors"
@@ -117,6 +118,10 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 	if err != nil {
 		return 1
 	}
+	logger = logger.With(
+		slog.String("git_sha", build.GitSHA),
+		slog.String("build_time", build.BuildTime),
+	)
 	defer func() {
 		if err := closeLogger(); err != nil {
 			logger.Error("failed to close logger", slog.Any("error", err))
